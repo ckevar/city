4193428bc61be5e5dd6c8d870a542ea88dbe060d
ckevar <ckevar@gmail.com>
2018-12-03 00:39:34 +0100

    modified:   include/vehicle.h

--- include/vehicle.h
+++ include/vehicle.h
@@ -1,9 +1,19 @@
 #ifndef VEHICLE_H
 #define VEHICLE_H 
 
-typedef struct vehicle_T
+typedef struct vehicle_
 {
-
+	int 		color;
+	double 		mass;
+	double 		friction;
+	double 		controlU;
+	distSens_t  *dsensor;
+	cam_t 		*camera;	
+	double		posXr;	 // real (or granted) position
+	double		posYr; 	// real (or granted) position
+	double		orientation;
+	double		velocity;
+	gps_t 		*gps;
 } vehicle_t;
 
 void *drive(vehicle_t *myVehicle);
