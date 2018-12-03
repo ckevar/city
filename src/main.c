744889ace83214fa58888cbe836ee6679221f578
ariannagavioli <gavioli.arianna@gmail.com>
2018-12-03 22:47:16 +0100

    modified:   src/main.c

--- src/main.c
+++ src/main.c
@@ -19,9 +19,15 @@ void initialize_graphics()
 
 	clear_to_color(screen, 8);		//grey background
 
-	int i;
-	for(i=0; i<n_blocks; i++){
-		rectfill(screen,(W+i+10),(H+i+20)/H,(W+i+20)/W,(H+i+10)/10,10);
+	/*Initializing a simple random map*/
+	int n_blocks_x, n_blocks_y;
+	n_blocks_x = W/(2*n_blocks);
+	n_blocks_y = H/(2*n_blocks);
+	int i,j;
+	for(i=0; i<n_blocks_x; i++){
+		for(j=0; j<n_blocks_y; j++){
+			rectfill(screen,(i*20+5),(j*15+10),(i*15+5),(j*20+10),10);
+		}
 	}
 }
 
@@ -29,7 +35,7 @@ int main(int argc, char const *argv[])
 {
 	allegro_init();
 
-	n_blocks = 10;	//for now
+	n_blocks = 30;	//for now
 
 	initialize_graphics();
 
