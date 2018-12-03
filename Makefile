a6019102882046a3d53664c44a1fa903da39f04e
ckevar <ckevar@gmail.com>
2018-12-03 17:52:39 +0100

    modified:   Makefile 	modified:   include/sensors.h

--- Makefile
+++ Makefile
@@ -21,6 +21,7 @@ $(BUILDDIR)/%.o: $(SRC)
 
 clean:
 	rm -r $(BUILDDIR) $(TARGET)
+
 git:
 	(git commit -a; git push)
 
--- include/sensors.h
+++ include/sensors.h
@@ -3,12 +3,18 @@
 
 typedef struct 
 {
-	
+	char	visible;
+	int		posX;
+	int 	posY;
+	double 	*distances;
 } dstsens_t;
 
 typedef struct
 {
-	
+	int		resX;
+	int		ResolutionY;
+	double	posX;
+	double	posY;
 } cam_t;
 
 typedef struct
