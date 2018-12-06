4d8d2146a9f209d84b6a188e810c2324fe83149d
Christian Alvarado <ckevar@gmail.com>
2018-12-06 22:59:41 +0100

    added sensors features

--- include/sensors.h
+++ include/sensors.h
@@ -5,6 +5,18 @@
 
 #include "vehicle.h"
 
+/** RANGEFINDER FEATURES **/
+#define SMIN 10
+#define SMAX 100
+#define STEP 1
+
+/** CAMERA FEATURES **/
+#define HRES 16
+#define VRES 9
+
+
+void initCam(cam_t *myCam, const int x, const int y);
+
 /**** REAL-TIME TASKS ****/
 void *getFrame(vehicle_t *myVehicle);
 void *getRangefinder(vehicle_t *myVehicle);
