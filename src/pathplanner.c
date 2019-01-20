b1cdd718ff134c16153a8566726d1cffa9a6abd5
ckevar <ckevar@gmail.com>
2019-01-21 00:35:15 +0100

    magic:   src/pathplanner.c

--- src/pathplanner.c
+++ src/pathplanner.c
@@ -287,17 +287,19 @@ char pathPlanner(vehicle_t *c) {
 		/******** CROSSROAD RESPONSE ********/
 		if (!c->isTime2Steer) {
 			keepCertainDistanceFromBlock(c, DST_FROM_LEFT_SIDEBLOCK);	// keep safe distance from blocks
+				
+				
+			if(imf.stCorner.N == 2) 
+				check2turnAround(c, &imf);
+				
 			if (imf.stCorner.N){
-				if(c->Vr > 20)	c->Vr = c->Vr-10;
-				//printf("%f\n", c->vel);
-				if(imf.stCorner.N== 2) {
-					check2turnAround(c, &imf);
+				if(c->Vr > 20) c->Vr = c->Vr - 10;
+				if (imf.stCorner.x[0] < DST_TO_STEER) {
+					chooseSteering(c, &imf);	// the vehicle will go left, right or straigh
+					genKInvX(c, &imf);			// generates basic parameters to compute the steering trajectory
 				}
 			}
-			if ((imf.stCorner.x[0] < DST_TO_STEER) && (imf.stCorner.N)) {
-				chooseSteering(c, &imf);	// the vehicle will go left, right or straigh
-				genKInvX(c, &imf);			// generates basic parameters to compute the steering trajectory
-			}
+			
 		}
 
 		if (c->isTime2Steer) 
